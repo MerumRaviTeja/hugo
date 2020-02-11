01486ea7fa87f1049415fc6f8416c326201af36f
merumraviteja <raviteja87731@gmail.com>
2020-02-11 22:16:59 +0530

    remove requirements

--- attedance/settings/common.py
+++ attedance/settings/common.py
@@ -6,9 +6,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = "top-secret!"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://attedanceapp.herokuapp.com/']
 
 
 # Application definition
